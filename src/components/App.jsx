a5fc638c56d1370046f99ca5a432f2ec273960f8
Volodymyr-Bednov <109081369+Volodymyr-Bednov@users.noreply.github.com>
2023-02-05 23:03:21 +0200

    step - 3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,73 @@
-import Feedback from './Feedback/Feedback';
-export const App = () => {
-  return (
-    <div>
-      <Feedback />
-    </div>
-  );
-};
+import { Component } from 'react';
+import { Section } from './Section/Section';
+import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
+import { Statistics } from './Statistics/Statistics';
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+    total: 0,
+    positivePercentage: 0,
+    isVisible: true,
+  };
+
+  LeaveFeedback = evt => {
+    if (evt.target.type !== 'button') return;
+    const selectGrade = evt.target.value;
+    this.setState(prevValue => {
+      return {
+        [selectGrade]: prevValue[selectGrade] + 1,
+      };
+    });
+    this.countTotalFeedback();
+  };
+
+  countTotalFeedback = () => {
+    this.setState(prevValue => {
+      const { good, neutral, bad } = prevValue;
+      return { total: good + neutral + bad };
+    });
+    this.countPositiveFeedbackPercentage();
+  };
+
+  countPositiveFeedbackPercentage() {
+    this.setState(prevValue => {
+      const { good, total } = prevValue;
+      return { positivePercentage: Math.round((good * 100) / total) };
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <Section
+          title={'Please leave feedback'}
+          children={<FeedbackOptions onLeaveFeedback={this.LeaveFeedback} />}
+        />
+        {/* <FeedbackOptions onLeaveFeedback={this.LeaveFeedback} /> */}
+
+        <Section
+          title={'Statistics'}
+          children={
+            <Statistics
+              good={this.state.good}
+              neutral={this.state.neutral}
+              bad={this.state.bad}
+              total={this.state.total}
+              positivePercentage={this.state.positivePercentage}
+            />
+          }
+        />
+
+        {/* <Statistics
+          good={this.state.good}
+          neutral={this.state.neutral}
+          bad={this.state.bad}
+          total={this.state.total}
+          positivePercentage={this.state.positivePercentage}
+        /> */}
+      </div>
+    );
+  }
+}
---
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -0,0 +1,93 @@
+import css from './Feedback.module.css';
+export const FeedbackOptions = ({ onLeaveFeedback }) => {
+  return (
+    <div>
+      <div className={css.grade} onClick={onLeaveFeedback}>
+        <button type="button" value="good">
+          Good
+        </button>
+
+        <button type="button" value="neutral">
+          Neutral
+        </button>
+
+        <button type="button" value="bad">
+          Bad
+        </button>
+      </div>
+    </div>
+  );
+};
+
+// import { Component } from 'react';
+// import css from './Feedback.module.css';
+
+// export class FeedbackOptions extends Component {
+//   state = {
+//     good: 0,
+//     neutral: 0,
+//     bad: 0,
+//     total: 0,
+//     positivePercent: 0,
+//     isVisible: true,
+//   };
+
+//   handlerGrade = evt => {
+//     if (evt.target.type !== 'button') return;
+//     const selectGrade = evt.target.value;
+//     this.setState(prevValue => {
+//       return {
+//         [selectGrade]: prevValue[selectGrade] + 1,
+//       };
+//     });
+//     this.countTotalFeedback();
+//   };
+
+//   countTotalFeedback = () => {
+//     this.setState(prevValue => {
+//       const { good, neutral, bad } = prevValue;
+//       return { total: good + neutral + bad };
+//     });
+//     this.countPositiveFeedbackPercentage();
+//   };
+
+//   countPositiveFeedbackPercentage() {
+//     this.setState(prevValue => {
+//       const { good, total } = prevValue;
+//       return { positivePercent: Math.round((good * 100) / total) };
+//     });
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <h2>Please leave feedback</h2>
+
+//         <div className={css.grade} onClick={this.handlerGrade}>
+//           <button type="button" value="good">
+//             Good
+//           </button>
+
+//           <button type="button" value="neutral">
+//             Neutral
+//           </button>
+
+//           <button type="button" value="bad">
+//             Bad
+//           </button>
+//         </div>
+
+//         <div>
+//           <h2>Statistics</h2>
+//           <ul>
+//             <li>Good: {this.state.good}</li>
+//             <li>Neutral: {this.state.neutral}</li>
+//             <li>Bad: {this.state.bad}</li>
+//             <li>Total: {this.state.total}</li>
+//             <li>Positive feedback: {this.state.positivePercent}%</li>
+//           </ul>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
