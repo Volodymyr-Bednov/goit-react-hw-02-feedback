b931a617f0170bbf38528233a320f062488d03a7
Volodymyr-Bednov <109081369+Volodymyr-Bednov@users.noreply.github.com>
2023-02-05 23:43:51 +0200

    spep - 4

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import { Component } from 'react';
 import { Section } from './Section/Section';
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 import { Statistics } from './Statistics/Statistics';
+import { Notification } from './Notification/Notification';
 export class App extends Component {
   state = {
     good: 0,
@@ -9,7 +10,7 @@ export class App extends Component {
     bad: 0,
     total: 0,
     positivePercentage: 0,
-    isVisible: true,
+    isVisible: false,
   };
 
   LeaveFeedback = evt => {
@@ -18,6 +19,7 @@ export class App extends Component {
     this.setState(prevValue => {
       return {
         [selectGrade]: prevValue[selectGrade] + 1,
+        isVisible: true,
       };
     });
     this.countTotalFeedback();
@@ -50,16 +52,19 @@ export class App extends Component {
         <Section
           title={'Statistics'}
           children={
-            <Statistics
-              good={this.state.good}
-              neutral={this.state.neutral}
-              bad={this.state.bad}
-              total={this.state.total}
-              positivePercentage={this.state.positivePercentage}
-            />
+            this.state.isVisible ? (
+              <Statistics
+                good={this.state.good}
+                neutral={this.state.neutral}
+                bad={this.state.bad}
+                total={this.state.total}
+                positivePercentage={this.state.positivePercentage}
+              />
+            ) : (
+              <Notification message={'There is no feedback'} />
+            )
           }
         />
-
         {/* <Statistics
           good={this.state.good}
           neutral={this.state.neutral}
